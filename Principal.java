a42a2959015df9e2d592e1ffc94aa434df92e73f
drmdrm95 <diego.a1995@hotmail.com>
2015-09-22 18:22:06 -0500

    principal main

---
+++ Principal.java
@@ -0,0 +1,18 @@
+
+package quienquieresermillonariofinal;
+
+
+
+public class Principal {
+
+   
+    
+    public static void main(String[] args) {
+        
+        //Se muestra el menú principal al ejecutar la aplicación
+        new Menu_Principal().setVisible(true);
+        
+        
+    } 
+    
+} 
